31f6a6165ac7f481ae8f9fe9d00a24a140bef330
5adiyah <sadiali@pdx.edu>
2021-01-28 01:48:02 -0800

    Update post schema

--- schemas/post.js
+++ schemas/post.js
@@ -50,16 +50,17 @@ export default {
                 {
                     type: 'block',
                     styles: [
+                        { title: 'Normal', value: 'normal' },
+                        { title: 'H1', value: 'h1' },
+                        { title: 'H2', value: 'h2' },
+                        { title: 'H3', value: 'h3' },
+                        { title: 'H4', value: 'h4' },
+                        { title: 'H5', value: 'h5' },
                         {title: 'Quote', value: 'blockquote'},
                     ]
                 },
-                {
-                    type: 'image'
-                },
-                {
-                    type: 'code'
-                }
-            ],
+                // Marks let you mark up inline text in the block editor.
+            ]
         }
     ]
 }
